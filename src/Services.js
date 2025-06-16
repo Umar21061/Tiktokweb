cfb2ef8b8c757174067ad6624a150adef9f80d8c
Umar <umerc21061@gmai.com>
2025-06-16 22:29:58 +0500

    Updated button styles and heading colors

--- src/Services.js
+++ src/Services.js
@@ -11,20 +11,22 @@ const Services = ({ homeRef, onBuyAccountClick, scrollToSection, refs }) => {
           {/* Left Side */}
           <Col md={7}>
             <h1 className="tiktokshop-title">TikTokShop</h1>
-            <h2 className="tiktokshop-title-2 mt-4">Account Monetization Made Easy</h2>
+            <h2 className="tiktokshop-title-2 mt-4">Account Monetization </h2>
             <p className="lead mt-3">
               We help creators monetize faster with trusted tools and audience growth services.
             </p>
             <p className="small-text mb-4">
-              Secure, real users. Fast delivery. 100% satisfaction guaranteed.
-            </p>
+  <span style={{ color: '#198754' }}>Secure, real users.</span>{' '}
+  <span style={{ color: '#dc3545' }}>Fast delivery. 100% satisfaction guaranteed.</span>
+</p>
+
             <div className="text-start">
               <Button
                 variant="danger"
                 className="get-started-btn"
                 onClick={() => scrollToSection(refs.homeRef)}
               >
-                🚀 Get Started
+                Get Started
               </Button>
             </div>
           </Col>
@@ -33,7 +35,7 @@ const Services = ({ homeRef, onBuyAccountClick, scrollToSection, refs }) => {
           <Col md={5}>
             <Card className="sale-offer-box text-white">
               <Card.Body>
-                <h4>Buy Trusted TikTok Accounts</h4>
+                <h4>Buy TikTok Accounts</h4>
                 <p>🚀 Secure Your TikTok Account Today  70% Discount for a Limited Time!</p>
                 <p>✅ All Countries TikTok Accounts Available UK, UAE, USA & More!</p>
                 <Button className="custom-btn" onClick={onBuyAccountClick}>
